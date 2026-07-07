1d66391e9c13a51458bee9208635da95e320e57a
Harsha <harxshz2004@gmail.com>
2026-07-07 22:29:23 +0530

    pattren's acr begins

---
+++ looping_territory/Pattren.java
@@ -0,0 +1,13 @@
+package looping_territory;
+
+public class Pattren {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
---
+++ looping_territory/Pattren3.java
@@ -0,0 +1,13 @@
+package looping_territory;
+
+public class Pattren3 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
