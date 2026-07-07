cf4679192178f997d1a32d0e38b6d6800b561fd5
Harsha <harxshz2004@gmail.com>
2026-07-07 22:49:36 +0530

    entering looping territory

--- Pattren5.java
+++ Pattren5.java
@@ -1,12 +1,11 @@
-package looping_territory;
-
-public class Pattren3 {
+public class Pattren5 {
     public static void main(String[] args) {
         for (int i = 5; i >= 1; i--) {
             for (int j = i; j >= 1; j--) {
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println(" ");
         }
+
     }
 }
