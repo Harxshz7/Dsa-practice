b20f9f161af8f16bfd39415a8ce54e76b024b416
Harsha <harxshz2004@gmail.com>
2026-07-16 21:47:37 +0530

    reversing string with out charAt()

---
+++ slide.java
@@ -0,0 +1,16 @@
+public class slide {
+    public static void main(String[] args) {
+        String str = "method";
+        char[] ch = str.toCharArray();
+        int i = 0, j = ch.length - 1;
+        while (i > j) {
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            i++;
+            j++;
+        }
+        System.out.println(new String(ch));
+    }
+
+}
