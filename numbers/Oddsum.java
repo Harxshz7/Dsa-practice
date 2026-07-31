22744412bd851f89fd18726ec597aba926bb24a2
Harsha <harxshz2004@gmail.com>
2026-07-31 22:52:16 +0530

    Add Oddsum program to calculate the sum of odd digits in a number

---
+++ numbers/Oddsum.java
@@ -0,0 +1,18 @@
+package numbers;
+
+public class Oddsum {
+    public static void main(String[] args) {
+        int no = 948216083;
+        int oddsum = 0;
+
+        while (no != 0) {
+            int rem = no % 10;
+            if (rem % 2 != 0) {
+                oddsum += rem;
+            }
+            no /= 10;
+        }
+
+        System.out.println("sum of even number digit in given number is " + oddsum);
+    }
+}
