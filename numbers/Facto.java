dff9911e28eb2810569c501fc28285cd4fdadc12
Harsha <harxshz2004@gmail.com>
2026-08-07 19:37:20 +0530

    Add Facto program to calculate the factorial of a number

---
+++ numbers/Facto.java
@@ -0,0 +1,14 @@
+package numbers;
+
+public class Facto {
+    public static void main(String[] args) {
+        int no = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= no; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial of " + no + " is " + fact);
+    }
+}
